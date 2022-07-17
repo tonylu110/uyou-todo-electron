3c622fdfe98dbc25f665871852b401ad675554c2
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-07-17 12:07:53 +0800

    Add: setting item

--- src/components/SettingList/SettingList.vue
+++ src/components/SettingList/SettingList.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="list">
-  
+    <div class="title-img">
+      <img src="/images/logo.png" alt="" />
+      <span>uyou ToDo v1.0.2</span>
+    </div>
+    <div class="setting-item lang-set" @click="() => langMenuShow = !langMenuShow">
+      <img src="/images/lang.png" alt="" class="lang-img" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const langMenuShow = ref(false)
 </script>
 
-<style scoped>
-.list {
-  background-color: #eee;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  overflow-y: scroll;
-  padding-top: 10px;
-}
+<style scoped lang="scss">
+@import './style.scss';
 </style>
