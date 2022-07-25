dd8a14a994f3cc9bb7985ea23e5aa76b4cc0e570
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-07-25 10:33:53 +0800

    Add: edit to sync

--- src/components/List/saveItemSet.ts
+++ src/components/List/saveItemSet.ts
@@ -7,6 +7,23 @@ const saveItemSet = (list: ITodoList[]) => {
     data: list
   }
   LocalStorage('set', localStorageSetTodoList)
+  const uid = localStorage.getItem('uid')
+  if (uid !== null && uid !== '') {
+    fetch('https://api.todo.uyou.org.cn/edittodo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        uid: uid,
+        data: JSON.stringify(localStorageSetTodoList)
+      })
+    }).then(res => {
+      return res.json()
+    }).then(res => {
+      console.log(res);
+    })
+  }
 }
 
 export default saveItemSet
