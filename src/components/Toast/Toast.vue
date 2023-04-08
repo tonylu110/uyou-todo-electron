7597b89061a444576218f5fbf80c71b396eae482
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-04-08 19:55:57 +0800

    Fix: toast style

--- src/components/Toast/Toast.vue
+++ src/components/Toast/Toast.vue
@@ -6,7 +6,7 @@
     c="#996b3d" bg="#fff6dc" shadow-xl
     animate-duration-300 animate-fill-mode-forwards animate-ease
     :top="center ? '50%' : ''"
-    :left="simpleMode ? '50%' : (center ? '50%' : 'calc(50% + 150px)')"
+    :left="center ? '50%' : (simpleMode ? '' : 'calc(50% + 150px)')"
     :translate="center ? '-50%' : '-x-50%'"
     :position="center ? 'absolute' : 'fixed'"
     :class="center ? '' : 'animate-toastShow'"
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-const props = withDefaults(defineProps<{
+withDefaults(defineProps<{
   msg: string
   center?: boolean
 }>(), {
