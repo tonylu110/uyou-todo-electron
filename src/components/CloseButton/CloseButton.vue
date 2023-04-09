d953b2a295612d5ef392f1dc666ab225754917da
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-04-09 08:37:27 +0800

    Change: close button to unocss

--- src/components/CloseButton/CloseButton.vue
+++ src/components/CloseButton/CloseButton.vue
@@ -5,47 +5,23 @@ const ipcRenderer = require('electron').ipcRenderer
 const route = useRoute()
 
 const close = () => {
-    ipcRenderer.send(`close-${(route.name as unknown as string)}`)
+  ipcRenderer.send(`close-${(route.name as unknown as string)}`)
 }
 </script>
 
 <template>
-    <div class="close-button" @click="close">
-        <span class="material-icons">close</span>
-    </div>
+  <div 
+    fixed right-7.5px top-7.5px
+    bg="#e5544b hover:#c95047 active:#99362f"
+    h-20px w-50px
+    flex items-center justify-center
+    rounded-5px 
+    no-drag
+    cursor-pointer
+    c-white
+    z-1
+    @click="close"
+  >
+    <div i-mdi:close-thick c-white text-12px ml-1px></div>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-.close-button {
-    position: fixed;
-    right: 7.5px;
-    top: 7.5px;
-    background-color: #e5544b;
-    height: 20px;
-    width: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 5px;
-    -webkit-app-region: no-drag;
-    cursor: pointer;
-    color: white;
-    z-index: 1;
-
-    &:hover {
-        background-color: #c95047;
-    }
-
-    &:active {
-        background-color: #99362f;
-    }
-
-    span {
-        color: white;
-        display: block;
-        font-size: 12px;
-        font-weight: bold;
-        margin-left: 1px;
-    }
-}
-</style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import { defineConfig, presetAttributify, presetUno, presetIcons } from 'unocss'
 
 export default defineConfig({
   presets: [
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
     presetAttributify(), 
     presetUno()
+  ],
+  rules: [
+    ['no-drag', { '-webkit-app-region': "no-drag" }],
+    ['drag', { '-webkit-app-region': "drag" }]
   ],
   theme: {
     animation: {
