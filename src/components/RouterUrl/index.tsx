30613d35def9966c22ed9436f7bcf4ffd90ca83f
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-04-19 16:06:17 +0800

    Add: drag route window

--- src/components/RouterUrl/index.tsx
+++ src/components/RouterUrl/index.tsx
@@ -1,17 +1,67 @@
-import { defineComponent } from "vue";
-import { useRoute } from "vue-router";
+import { defineComponent, onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 export default defineComponent({
   setup() {
     const route = useRoute()
+    const router = useRouter()
+
+    const routeUrl = ref('')
+    const toUrl = () => {
+      router.push(routeUrl.value)
+      routeUrl.value = ''
+    }
+
+    const body = ref(null)
+
+    onMounted(() => {
+      const x = ref(0)
+      const y = ref(0)
+
+      const bodyDOM = body.value as unknown as HTMLElement
+
+      const dragWindow = (ev: MouseEvent) => {
+        bodyDOM.style.top = ev.clientY - y.value + 'px'
+        bodyDOM.style.left = ev.clientX - x.value + 'px'
+      }
+
+      const mouseMove = (ev: MouseEvent) => {
+        bodyDOM.style.transform = 'translate(0px)'
+        x.value = ev.offsetX
+        y.value = ev.offsetY
+        bodyDOM.addEventListener('mousemove', dragWindow)
+      }
+
+      bodyDOM.addEventListener('mousedown', mouseMove)
+      bodyDOM.addEventListener('mouseup', () => {
+        bodyDOM.removeEventListener('mousemove', dragWindow)
+      })
+    })
 
     return () => (
       <div
         fixed top="50px" left="50%" translate="x-[-50%]"
         z-1000 bg-white p-x-20px p-y-10px shadow-item
-        rounded-24
+        rounded-xl no-drag
+        flex="~ col" items-center
+        ref={body}
       >
-        {route.fullPath} [name: {route.name}]
+        <div>{route.fullPath} [name: {route.name}]</div>
+        <div flex mt-10px>
+          <input 
+            p-10px outline-primary-d mr-10px
+            rounded-5px border-none bg="black/10"
+            type="text" 
+            v-model={routeUrl.value} 
+          />
+          <button
+            bg="active:primary-a primary-d" c-white border-none
+            rounded-5px p-10px cursor-pointer
+            onClick={toUrl}
+          >
+            GO
+          </button>
+        </div>
       </div>
     )
   }
