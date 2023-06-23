ba9af789b62cc8e034b1056683b577068e218df6
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-06-23 20:09:10 +0800

    pref: change another window close button

--- src/components/CloseButton/index.tsx
+++ src/components/CloseButton/index.tsx
@@ -3,6 +3,7 @@ import { useRoute } from 'vue-router'
 
 export default defineComponent({
   setup() {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
     const ipcRenderer = require('electron').ipcRenderer
     const route = useRoute()
 
@@ -12,18 +13,15 @@ export default defineComponent({
 
     return () => (
       <div
-        fixed right="7.5px" top="7.5px"
-        bg="error-d hover:error-h active:error-a"
-        h-20px w-50px
+        fixed right-12px top-12px
+        bg="black/10 hover:error-d active:error-a"
         flex items-center justify-center
-        rounded-5px
-        no-drag
-        cursor-pointer
-        c-white
-        z-1
+        no-drag cursor-pointer c-white z-1
+        w-13px h-13px p-6px rounded-full
+        className="group"
         onclick={close}
       >
-        <div i-mdi:close-thick c-white text-12px ml-1px></div>
+        <div i-mdi:close-thick c="#555 group-hover:white group-active:white" text-13px></div>
       </div>
     )
   },
