0fb60e3677cf085f2608b22ab56d3013dd2b1e83
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-08-26 09:49:34 +0800

    refactor: icons

---
+++ src/components/ListMenu/MenuItem/Icons/icons.ts
@@ -0,0 +1,14 @@
+const icons = [
+  'i-ph:radio-button-bold',
+  'i-ph:alien-bold',
+  'i-ph:baby-bold',
+  'i-ph:backpack-bold',
+  'i-ph:basketball-bold',
+  'i-ph:buildings-bold',
+  'i-ph:currency-circle-dollar-bold',
+  'i-ph:hamburger-bold',
+  'i-ph:house-bold',
+  'i-ph:nut-bold',
+]
+
+export default icons
--- src/components/ListMenu/MenuItem/Icons/index.tsx
+++ src/components/ListMenu/MenuItem/Icons/index.tsx
@@ -1,70 +1,21 @@
+import icons from './icons'
+
 const Icons: SetupFC = () => {
   const emit = defineEmits<{
     setIcon: [icon: string]
   }>()
 
   return (
-    <div p-10px flex="~ gap-5px wrap" max-w-150px>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:radio-button-bold')}
-      >
-        <div i-ph:radio-button-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:alien-bold')}
-      >
-        <div i-ph:alien-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:baby-bold')}
-      >
-        <div i-ph:baby-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:backpack-bold')}
-      >
-        <div i-ph:backpack-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:basketball-bold')}
-      >
-        <div i-ph:basketball-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:buildings-bold')}
-      >
-        <div i-ph:buildings-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:currency-circle-dollar-bold')}
-      >
-        <div i-ph:currency-circle-dollar-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:hamburger-bold')}
-      >
-        <div i-ph:hamburger-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:house-bold')}
-      >
-        <div i-ph:house-bold block />
-      </div>
-      <div
-        p-5px rounded-5px bg="black/10 active:black/20"
-        onClick={() => emit('setIcon', 'i-ph:nut-bold')}
-      >
-        <div i-ph:nut-bold block />
-      </div>
+    <div p-7px flex="~ gap-5px wrap" max-w-150px>
+      {icons.map((item, index) => (
+        <div
+          p-5px rounded-5px bg="black/10 active:black/20"
+          key={index}
+          onClick={() => emit('setIcon', item)}
+        >
+          <div className={item} block />
+        </div>
+      ))}
     </div>
   )
 }
