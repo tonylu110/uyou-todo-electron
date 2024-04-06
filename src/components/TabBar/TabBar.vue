55f3a431bc214e68330c9936322a275de2593951
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2024-04-06 12:55:25 +0800

    feat: change show today time

--- src/components/TabBar/TabBar.vue
+++ src/components/TabBar/TabBar.vue
@@ -10,6 +10,7 @@ import { isLinux, isMac, isWindows10OrAfter } from '../../util/os'
 import type { cateItem } from '../ListMenu/ICateItem'
 import changeCate from '../ListMenu/changCate'
 import windowButtons from './windowButtons'
+import { useI18n } from 'vue-i18n'
 
 withDefaults(defineProps<{
   title?: string
@@ -33,6 +34,8 @@ const emits = defineEmits<{
   (e: 'rightClick'): void
   (e: 'leftClick'): void
 }>()
+
+const { t } = useI18n()
 
 const router = useRouter()
 const route = useRoute()
@@ -95,6 +98,8 @@ const isBlur = (localStorage.getItem('menuBlur') === 'true' || localStorage.getI
 
 const useCustColor = ref(localStorage.getItem('useCustColor') === 'true')
 const showColor = ref(false)
+
+const showToday = ref(false)
 
 const isDark = usePreferredDark()
 
@@ -204,6 +209,50 @@ watchEffect(() => {
         <window-buttons v-if="!systemTitleShow" />
       </div>
       <div flex mt-12px :h="rightImgShow ? '' : '30px'">
+        <VDropdown
+          v-model:shown="showToday"
+          :distance="12"
+          placement="bottom"
+        >
+          <div
+            v-if="route.query.listName === 'today'"
+            bg="black/10 hover:black/20 dark:#999/10 dark:hover:#999/20"
+            p-5px w-20px rounded-5px no-drag cursor-pointer
+            @click="showToday = true"
+          >
+            <div i-ph:clock-bold text-20px c="#555 dark:#bbb" block />
+          </div>
+          <template #popper>
+            <div flex="~ wrap col">
+              <div
+                class="group"
+                flex items-center p-3
+                border-b="1px solid #ddd dark:#333"
+                bg="hover:black/5 active:primary-d"
+              >
+                <div i-ph:check-bold c-primary-d mr-3/>
+                <span select-none group-active:c-white>{{ t('todayShow.creatTime') }}</span>
+              </div>
+              <div
+                class="group"
+                flex items-center p-3 
+                border-b="1px solid #ddd dark:#333"
+                bg="hover:black/5 active:primary-d"
+              >
+                <div i-ph:check-bold c-primary-d mr-3/>
+                <span select-none group-active:c-white>{{ t('todayShow.remindTime') }}</span>
+              </div>
+              <div
+                class="group"
+                flex items-center p-3
+                bg="hover:black/5 active:primary-d"
+              >
+                <div i-ph:check-bold c-primary-d mr-3/>
+                <span select-none group-active:c-white>{{ t('todayShow.allTime') }}</span>
+              </div>
+            </div>
+          </template>
+        </VDropdown>
         <VDropdown
           v-model:shown="showColor"
           :distance="12"
