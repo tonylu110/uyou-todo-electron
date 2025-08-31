fd7fa3dad008314335b65183a06753e87f429e70
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2025-08-31 13:16:07 +0800

    fix: router

--- src/router/mainRouter/index.ts
+++ src/router/mainRouter/index.ts
@@ -2,8 +2,8 @@ import type { RouteRecordRaw } from 'vue-router'
 import Home from '../../pages/Home.vine.ts'
 import NoteUI from '../../pages/NoteUI.vine.ts'
 import Other from '../../pages/Other.vine.ts'
-import Setting from '../pages/Settings/Setting.vue'
-import SettingsSimple from '../pages/SettingsSimple.vue'
+import Setting from '../../pages/Settings/Setting.vue'
+import SettingsSimple from '../../pages/SettingsSimple.vue'
 
 const mainRouter: Readonly<RouteRecordRaw[]> = [
   {
